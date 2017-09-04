b7bb4454809c23a31ce49757b228c8119c0541fd
DESKTOP-7SE89SC\hac <hac0102@naver.com>
2017-09-04 18:40:31 +0900

    상속추상을 통해 각기다른 db요청,연결등등 처리

--- src/main/java/kr/co/test/user/dao/UserDao.java
+++ src/main/java/kr/co/test/user/dao/UserDao.java
@@ -1,7 +1,6 @@
 package kr.co.test.user.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,8 +8,9 @@ import java.sql.SQLException;
 import kr.co.test.user.domain.User;
 
 
-
-public class UserDao {
+//각각 다른 db, db커넥션을 사용할때 
+//상속이나 추상을 이용하여, 핵심코드는제외하고 클래스 바이너리파일만 제공
+public abstract class UserDao {
 	
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		
@@ -65,12 +65,29 @@ public class UserDao {
 
 	
 	//db연결과 관련된부분에 변경이 일어났을경우 이메소드만 바꿔주면 된다.
-	public Connection getConnection() throws ClassNotFoundException, SQLException{
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hr", "hr");
+	//각각 다른 db, db커넥션을 사용할때 
+	//상속이나 추상을 이용하여, 핵심코드는제외하고 클래스 바이너리파일만 제공
+	public abstract Connection getConnection() throws ClassNotFoundException, SQLException; 
+	
+//	{
+//		Class.forName("oracle.jdbc.driver.OracleDriver");
+//		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hr", "hr");
+//		
+//		return con;
+//	}
 		
-		return con;
+	public class NUserDao extends UserDao {
+		public Connection getConnection() throws ClassNotFoundException, SQLException {
+			//N사 DB 커넥션 생성코드
+			return null;
+		} 
 	}
-		
+	
+	public class DUserDao extends UserDao {
+		public Connection getConnection() throws ClassNotFoundException, SQLException {
+			//D사 DB 커넥션 생성코드
+			return null;
+		}
+	}
 
 }
